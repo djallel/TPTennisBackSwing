a1828694180a395a988ded6103f069b2978fef85
Tabet <b_tabet@esi.dz>
2022-01-13 16:13:20 +0100

    table + bouton

--- src/Reg/reg.java
+++ src/Reg/reg.java
@@ -4,4 +4,19 @@ import javax.swing.*;
 
 public class reg {
     private JLabel studentRegistrationLabel;
+    private JTextField textFieldStudentName;
+    private JTextField textFieldMobile;
+    private JTextField textFieldCourse;
+    private JButton editButton;
+    private JButton addButton;
+    private JButton deleteButton;
+    private JLabel studentNameLabel;
+    private JLabel mobileLabel;
+    private JLabel courseLabel;
+    private JPanel PanelOfTable;
+    private JTable table1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
